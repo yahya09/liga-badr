9ecd8f84296f8a75564d908482267db5f5697703
Yahya Muhammad <yahya@badr-interactive.com>
2017-02-05 19:19:46 +0700

    try using telepot example

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print "Hello world!"
+import telepot
+import time
+from pprint import pprint
+
+bot = telepot.Bot('169195335:AAHlZK-qs-gTCaubLevcxZ5jPLRrcX13Ftc')
+
+last_msg_id = 1790
+
+def handle(msg):
+    pprint(msg)
+
+bot.message_loop(handle)
+
+while 1:
+    time.sleep(10)
