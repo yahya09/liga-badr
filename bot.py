aa6b32e0ff2827d8d08d0a9df3416b0e0df11664
Yahya Muhammad <yahya@badr-interactive.org>
2017-02-06 18:48:11 +0700

    data from challonge

--- bot.py
+++ bot.py
@@ -2,23 +2,48 @@ import sys
 import time
 import telepot
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+import json
+
+lastMsgId = 0
+
+users = []
+
+with open('data.json', 'r') as f:
+    s = f.read()
+    users = json.loads(s);
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
+    buttons = []
+    btns = []
+    count = 0
+    for user in users:
+        count += 1
+        btns.append(InlineKeyboardButton(text = user['name'], callback_data = str(user['id'])))
+        if count % 3 == 0:
+            buttons.append(btns)
+            btns = []
+    #add last btns if present
+    if len(btns) > 0:
+        buttons.append(btns)
 
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                   [InlineKeyboardButton(text='Press me', callback_data='press')],
-               ])
-
-    bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    msg = bot.sendMessage(chat_id, 'Pilih peserta', reply_markup=keyboard)
+    global lastMsgId
+    lastMsgId = msg['message_id']
 
 def on_callback_query(msg):
     query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, from_id, query_data)
 
+    # if lastMsgId > 0:
+    bot.editMessageReplyMarkup(telepot.message_identifier(msg['message']))
+    # else:
+    #     print('no last message id')
     bot.answerCallbackQuery(query_id, text='Got it')
 
-TOKEN = sys.argv[1]  # get token from command-line
+#TOKEN = sys.argv[1]  # get token from command-line
+TOKEN = '169195335:AAHlZK-qs-gTCaubLevcxZ5jPLRrcX13Ftc'
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop({'chat': on_chat_message,
