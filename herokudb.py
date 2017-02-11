308c7d0052e0fcf18e86a1d5c42ed67413c17478
Yahya Muhammad <yahya@badr-interactive.com>
2017-02-11 07:13:46 +0700

    updateScore done

--- herokudb.py
+++ herokudb.py
@@ -44,7 +44,8 @@ def updateScore(players):
     match = cursor.fetchone()
     print(match)
     #update team history
-    updateTeamHistory(players)
+    updateTeamHistory(players,
+                      {'match_id':match[0], 'player1_id':player1[0], 'player2_id':player2[0]})
     #update match record
     updated_match = updateRecord([player1, player2], [players[0][2], players[1][2]], match)
     #update standings
@@ -58,52 +59,53 @@ match record is also db row (tuple)
 """
 def updateStandings(players, match_record):
     cursor = conn.cursor()
+    print('standing before:', players)
     #who wins? update w-d-l count, and total points
+    player1 = {'win': players[0][2], 'draw': players[0][3], 'lose': players[0][4],
+               'goals': players[0][5], 'conceded': players[0][6], 'points': players[0][7]}
+    player2 = {'win': players[1][2], 'draw': players[1][3], 'lose': players[1][4],
+               'goals': players[1][5], 'conceded': players[1][6], 'points': players[1][7]}
     if match_record[5] == players[0][0]:    #first player win
-        players[0][2] += 1
-        players[0][7] += 3
-        players[1][4] += 1
+        player1['win'] += 1
+        player1['points'] += 3
+        player2['lose'] += 1
     elif match_record[5] == players[1][0]:    #second player win
-        players[0][4] += 1
-        players[1][2] += 1
-        players[1][7] += 3
+        player2['win'] += 1
+        player2['points'] += 3
+        player1['lose'] += 1
     else:    #if draw
-        players[0][3] += 1
-        players[1][3] += 1
+        player1['draw'] += 1
+        player2['draw'] += 1
         #points
-        players[0][7] += 1
-        players[1][7] += 1
+        player1['points'] += 1
+        player2['points'] += 1
 
     #update goals, check who is player1
     if match_record[1] == players[0][0]:
         #scored
-        players[0][5] += match_record[3]
-        players[1][5] += match_record[4]
+        player1['goals'] += match_record[3]
+        player2['goals'] += match_record[4]
         #conceded
-        players[0][6] += match_record[4]
-        players[1][6] += match_record[3]
+        player1['conceded'] += match_record[4]
+        player2['conceded'] += match_record[3]
     else:
-        players[0][5] += match_record[4]
-        players[1][5] += match_record[3]
+        player1['goals'] += match_record[4]
+        player2['goals'] += match_record[3]
         #conceded
-        players[0][6] += match_record[3]
-        players[1][6] += match_record[4]
-
-
+        player1['conceded'] += match_record[3]
+        player2['conceded'] += match_record[4]
 
     query = ("UPDATE player_result SET "
-        "win_count=%s, draw_count=%s, lose_count=%s, "
-        "goals_count=%s, conceded_count=%s, total_points=%s "
-        "WHERE id=%s;")
+             "win_count=%s, draw_count=%s, lose_count=%s, "
+             "goals_count=%s, conceded_count=%s, total_points=%s "
+             "WHERE id=%s;")
     #first player
-    # cursor.execute(query, 
-    #     (players[0][2], players[0][3], players[0][4], 
-    #         players[0][5], players[0][6], players[0][7], players[0][0]))
-    # cursor.execute(query, 
-    #     (players[1][2], players[1][3], players[1][4], 
-    #         players[1][5], players[1][6], players[1][7], players[1][0]))
-    # conn.commit()
-    print(players)
+    cursor.execute(query, (player1['win'], player1['draw'], player1['lose'],
+                           player1['goals'], player1['conceded'], player1['points'], players[0][0]))
+    cursor.execute(query, (player2['win'], player2['draw'], player2['lose'],
+                           player2['goals'], player2['conceded'], player2['points'], players[1][0]))
+    conn.commit()
+    print('standing after:', [player1, player2])
     return True
 
 
@@ -113,32 +115,38 @@ score is [goal1, goal2]
 match record is db row (tuple)
 """
 def updateRecord(players, score, match_record):
+    print(players)
+    goal1 = 0
+    goal2 = 0
     #check who player1
     if match_record[1] == players[0][0]:    #first player is player1
-        match_record[3] = score[0]
-    elif match_record[2] == players[1][0]:    #second player is player1
-        match_record[4] = score[0]
+        goal1 = score[0]
+        goal2 = score[1]
+    elif match_record[1] == players[1][0]:    #second player is player1
+        goal2 = score[0]
+        goal1 = score[1]
     else:
         return "Wrong match record"
 
     #determine winner, null if draw
     winner = 'NULL'
     if score[0] > score[1]:
-        match_record[5] = players[0][0];
+        winner = players[0][0]
     elif score[0] < score[1]:
-        match_record[5] = players[1][0];
+        winner = players[1][0]
 
-    match_record[6] = 'complete'
+    state = 'complete'
     cursor = conn.cursor()
     query = ("UPDATE match_records SET "
-        "player1_goals=%s, player2_goals=%s, winner=%s, status='%s'"
-        "WHERE id=%s;")
-    # cursor.execute(query, (match_record[3], match_record[4], match_record[5], match_record[6], match_record[0]))
-    # conn.commit()
-    print(match_record)
-    return match_record
+             "player1_goals=%s, player2_goals=%s, winner=%s, status=%s"
+             "WHERE id=%s;")
+    cursor.execute(query, (goal1, goal2, winner, state, match_record[0]))
+    conn.commit()
+    new_record = (match_record[0], match_record[1], match_record[2], goal1, goal2, winner, state)
+    print("new record:", new_record)
+    return new_record
 
-def updateTeamHistory(players):
+def updateTeamHistory(players, match_data):
     #match result
     player1_result = "draw"
     player2_result = "draw"
@@ -150,9 +158,9 @@ def updateTeamHistory(players):
         player2_result = "win"
 
     cursor = conn.cursor()
-    query = "INSERT INTO player_team_history(player_id, match_id, team, match_result) VALUES (%s, %s, '%s', '%s');"
-    # cursor.execute(query, (player1_id, match_id, players[0][1].lower(), player1_result))
-    # cursor.execute(query, (player2_id, match_id, players[1][1].lower(), player2_result))
-    # conn.commit()
+    query = "INSERT INTO player_team_history(player_id, match_id, team, match_result) VALUES (%s, %s, %s, %s);"
+    cursor.execute(query, (match_data['player1_id'], match_data['match_id'], players[0][1].lower(), player1_result))
+    cursor.execute(query, (match_data['player2_id'], match_data['match_id'], players[1][1].lower(), player2_result))
+    conn.commit()
     print([player1_result, player2_result])
     return True
